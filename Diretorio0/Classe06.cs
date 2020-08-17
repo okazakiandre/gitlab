f4af97669a0da0a00659641cf3bf7208b9f50ab1
André Okazaki <asokazaki@gmail.com>
2020-08-17 16:08:40 -0300

    Reintegração das novas classes (#1)
    * Criação de novas classes
    
    * Correção nos valores das novas classes
    
    Co-authored-by: Andre Okazaki <andreok@libertyseguros.com.br>

---
+++ Diretorio0/Classe06.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitLab
+{
+    public class Classe06
+    {
+        public int Metodo6()
+        {
+            return 6;
+        }
+
+        public int Metodo6V5()
+        {
+            return Metodo6() * 5;
+        }
+    }
+}
