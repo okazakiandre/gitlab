6bdefe63df81de5f4ddafb675c1da992e5904c93
Andre Okazaki <andreok@libertyseguros.com.br>
2020-08-17 16:01:47 -0300

    Criação de novas classes

---
+++ Diretorio2/Classe26.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitLab
+{
+    public class Classe26
+    {
+        public int Metodo26()
+        {
+            return 26;
+        }
+
+        public int Metodo26V4()
+        {
+            return Metodo26() * 4;
+        }
+    }
+}
